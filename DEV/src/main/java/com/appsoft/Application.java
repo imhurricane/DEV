2476b8920106ff9628c18165a9e1caac024a7c94
hurri <imhurricane@163.com>
2019-08-09 15:57:12 +0800

    测试

--- DEV/src/main/java/com/appsoft/Application.java
+++ DEV/src/main/java/com/appsoft/Application.java
@@ -19,7 +19,7 @@ public class Application{
 	public static void main(String[] args) { 
 		SpringApplication.run(Application.class, args);
 	}
-	
+	 
 	@Bean 
 	public HttpMessageConverters fastJsonHttpMessageConverters() {
 		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
